76912672392b5d8a81db77a69504523c0e0acd1c
katsuikeda <128643018+katsuikeda@users.noreply.github.com>
2023-07-18 03:51:47 -0700

    Layout, Styling, and restructuring of the codebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Dagre from "@dagrejs/dagre";
-import React, { useCallback } from "react";
-import ReactFlow, { ReactFlowProvider, Panel, useNodesState, useEdgesState, useReactFlow, Controls } from "reactflow";
+import React, { useCallback, useState } from "react";
+import ReactFlow, { ReactFlowProvider, Panel, useNodesState, useEdgesState, useReactFlow, Controls, MarkerType } from "reactflow";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 
-import { initialNodes, initialEdges } from "./components/nodes-edges.jsx";
+import artists from "./assets/data/artists.json";
+import { initialNodes, initialEdges, createFeaturedArtistsList } from "./components/nodes-edges.jsx";
+
 import "reactflow/dist/style.css";
 
 const g = new Dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
@@ -30,6 +33,8 @@ const LayoutFlow = () => {
     const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
     const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
+    const [toggled, setToggled] = useState(false);
+
     const onLayout = useCallback(
         (direction) => {
             const layouted = getLayoutedElements(nodes, edges, { direction });
@@ -44,11 +49,54 @@ const LayoutFlow = () => {
         [nodes, edges]
     );
 
+    const updateNodesAndEdges = (artistId) => {
+        const featuredArtistsList = createFeaturedArtistsList(artists, artistId, 2);
+        const nodes = featuredArtistsList.map((artist, index) => ({
+            id: artist.id.toString(),
+            data: { label: artist.name },
+            position: { x: index * 200, y: 0 },
+        }));
+        const edges = featuredArtistsList
+            .map((artist) =>
+                artist.features.map((feature) => ({
+                    id: `e${artist.id}-${feature}`,
+                    source: artist.id.toString(),
+                    target: feature.toString(),
+                    markerEnd: {
+                        type: MarkerType.ArrowClosed,
+                        color: "#FDE047",
+                    },
+                }))
+            )
+            .flat();
+        setNodes(nodes);
+        setEdges(edges);
+    };
+
+    const createMenuItem = (artists) => {
+        return artists.map((artist) => (
+            <MenuItem key={artist.id} component={<div onClick={() => updateNodesAndEdges(artist.id)}></div>}>
+                {artist.name}
+            </MenuItem>
+        ));
+    };
+
     return (
-        <ReactFlow style={{ background: "violet" }} nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} fitView>
+        <ReactFlow nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} fitView>
+            <div style={{ display: "flex", height: "100vh", minHeight: "600px" }}>
+                <Sidebar style={{ zIndex: 100 }} onBackdropClick={() => setToggled(false)} toggled={toggled} breakPoint='all'>
+                    <Menu>{createMenuItem(artists)}</Menu>
+                </Sidebar>
+            </div>
             <Panel position='top-right'>
                 <button onClick={() => onLayout("TB")}>vertical layout</button>
                 <button onClick={() => onLayout("LR")}>horizontal layout</button>
+                <button onClick={() => setToggled(!toggled)}>Choose Artist</button>
+            </Panel>
+            <Panel position='bottom-center'>
+                <a href='https://katsuyukiikeda.com/' target='_blank'>
+                    <p>&copy; 2023 Katsuyuki Ikeda</p>
+                </a>
             </Panel>
             <Controls />
         </ReactFlow>
--- src/components/nodes-edges.jsx
+++ src/components/nodes-edges.jsx
@@ -1,18 +1,20 @@
+import { MarkerType } from "reactflow";
 import artists from "../assets/data/artists.json";
 
 export const createFeaturedArtistsList = (artists, artistId, level) => {
     const artist = artists.find((artist) => artist.id === artistId);
     if (level === 0) return [artist];
     const featuredArtists = artist.features.map((featuredArtist) => createFeaturedArtistsList(artists, featuredArtist, level - 1));
-    return [artist, ...featuredArtists.flat()];
+    // remove duplicates and return the flattened array
+    return [...new Set([artist, ...featuredArtists.flat()])];
 };
 
-const featuredArtistsList = createFeaturedArtistsList(artists, 0, 2);
+const featuredArtistsList = createFeaturedArtistsList(artists, 3, 2);
 
 export const initialNodes = featuredArtistsList.map((artist, index) => ({
     id: artist.id.toString(),
     data: { label: artist.name },
-    position: { x: 0, y: index * 100 },
+    position: { x: index * 200, y: 0 },
 }));
 
 export const initialEdges = featuredArtistsList
@@ -21,6 +23,11 @@ export const initialEdges = featuredArtistsList
             id: `e${artist.id}-${feature}`,
             source: artist.id.toString(),
             target: feature.toString(),
+            type: "default",
+            markerEnd: {
+                type: MarkerType.ArrowClosed,
+                color: "#FDE047",
+            },
         }))
     )
     .flat();
